8e3853babe4397235d3c6ce6a93111606a82c1dd
Mocarafee <harif.rp05@gmail.com>
2020-08-06 17:20:58 +0700

    kenzo: ancientos bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_kenzo.mk \
-    $(LOCAL_DIR)/aosp_kenzo.mk
+    $(LOCAL_DIR)/ancient_kenzo.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_kenzo-userdebug \
-    aosp_kenzo-eng
+   ancient_kenzo-userdebug \
+   ancient_kenzo-eng
